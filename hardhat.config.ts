31c7046295d9cc9b59e5e6a64e6eae2ff376f332
Michael Teixeira <meixeira@gmail.com>
2024-08-04 14:26:43 -0400

    Add Infura endpoint for Linea network to Hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
+import "dotenv/config";
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    linea_sepolia: {
+      url: `https://linea-sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    linea_mainnet: {
+      url: `https://linea-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
